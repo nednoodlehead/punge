7ed1249144c630eb43832379053fe0de51bd1fc8
NedNoodleHead <spencer.braak@gmail.com>
2023-04-19 17:44:21 -0700

    restructured database

---
+++ src/db/create_db.rs
@@ -0,0 +1,48 @@
+use rusqlite::{Connection, Params, params};
+use crate::playliststructs::DatabaseErrors;
+// Creates the file with the two default tables :D
+
+pub fn create_table_defaults() -> Result<(), DatabaseErrors>{
+    let conn = Connection::open("main.db")?;
+    conn.execute(
+            "CREATE TABLE main (
+            title TEXT,
+            author TEXT,
+            album TEXT,
+            features TEXT,
+            length SMALLINT,
+            savelocationmp3 TEXT,
+            savelocationjpg TEXT,
+            datedownloaded DATE,
+            lastlistenedto DATE,
+            ischild BOOL,
+            uniqueid TEXT PRIMARY KEY,
+            plays SMALLINT,
+            weight SMALLINT
+            )",
+            params![]
+        )?;
+        conn.execute(
+            "CREATE TABLE metadata (
+            title TEXT,
+            description TEXT,
+            thumbnail TEXT,
+            datecreated DATE,
+            songcount SMALLINT,
+            totaltime TEXT,
+            isautogen BOOL,
+            playlist_id TEXT
+            )",
+            params![]
+        )?;
+        conn.execute(
+            "CREATE TABLE playlist_relations (
+            playlist_id TEXT,
+            song_id TEXT
+            )",
+            params![]
+        )?;
+        Ok(())
+
+}
+
