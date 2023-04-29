8c078ec5c4061d56fea7fe9687152a8de7c99055
NedNoodleHead <spencer.braak@gmail.com>
2023-04-28 20:35:46 -0700

    updated youtube interface (videos now download)

--- src/utils/youtube_interface.rs
+++ src/utils/youtube_interface.rs
@@ -1,11 +1,12 @@
 // purpose of this file is to download youtube videos and name them appropriately. it calls
 // various functions from "decide_youtube.rs" to determine the title, author and album
 
-
+use pyo3::prelude::*;
 use rustube::Video;
 use std::fs;
-use pyo3::prelude::*;
 use std::process::Command;
+use rusqlite;
+use rusqlite::params;
 
 // #[path = "./decide_youtube.rs"]
 // mod decide;
@@ -15,8 +16,7 @@ use crate::playliststructs::{Playlist, PungeMusicObject};
 mod playlist;
 
 mod decide_youtube;
-use decide_youtube::{begin_single, begin_playlist};
-
+use decide_youtube::{begin_playlist, begin_single};
 
 // this is the function exposed to the rest of the app. It takes in the youtube link
 pub fn intro(link: &str) {
@@ -24,12 +24,18 @@ pub fn intro(link: &str) {
     if link.contains("list=") {
         let vid = playlist_parse(link);
         begin_playlist(vid)
-    }
-    else {
+    } else {
         let vid = single_parse(link);
-        let (artist,title,album) = begin_single(vid);
-        println!("{} | {} | {}", artist, title, album)
+        begin_single(vid);
     };
+}
+
+pub fn check_if_exists(uniqueid: String) {
+    // checks if the given unique id is found inside the main table. aka: has it been downloaded?
+    let conn = rusqlite::Connection::open("main.db").unwrap();
+    let res: String = conn.query_row("SELECT EXISTS(SELECT 1 FROM main WHERE uniqueid=?)", params![uniqueid], |row| row.get(0))
+        .unwrap();
+    println!("res: {}", res)
 
 
 }
@@ -40,7 +46,6 @@ fn playlist_parse(playlist_link: &str) -> Playlist {
     playlist
 }
 
-
 fn single_parse(single_link: &str) -> rustube::blocking::video::Video {
     // create a url type, used to create the video type
     let url = rustube::url::Url::parse(single_link).unwrap();
@@ -49,30 +54,4 @@ fn single_parse(single_link: &str) -> rustube::blocking::video::Video {
     let info = vid.video_details();
     vid
 }
-
-pub fn opus_to_mp3(file: &str, mut name: String) {
-    /// we are assuming that the order is:
-    /// download video
-    /// convert to mp3
-    /// rename / chop
-    /// so we should have the original name of the file from here. name should include
-    let mut directory_prefix: String = String::from("f:/"); // this should come from the json file that holds the directory of where mp3s and thumbnails are kept
-    if !directory_prefix.ends_with("\\") || !directory_prefix.ends_with("/") {
-        directory_prefix = directory_prefix + "/"
-    }
-    // if there is no .mp3 on the end of the filename, we need to add it
-    if !name.to_lowercase().ends_with(".mp3") {
-        name.to_owned();
-        name += ".mp3";
-    }
-    let new_path_complete = directory_prefix + name.as_str();
-    // call ffmpeg to turn the old webm file into an mp3 file
-    let x = Command::new("ffmpeg.exe").args(&["-i", file, "-vn", "-c:a", "libmp3lame", "-b:a", "192k",
-        new_path_complete.as_str()]).output();
-    println!("ffmpeg status: {:?}", x.unwrap());
-}
-
-
-
-
 
