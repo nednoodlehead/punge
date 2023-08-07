92bc9e60a6cf9b71a65a946cb21dd8e1072ff23e
NedNoodleHead <spencer.braak@gmail.com>
2023-08-06 19:41:24 -0700

    Moved program commands (makes more sense here) and added pages enum

--- src/gui/messages.rs
+++ src/gui/messages.rs
@@ -1,6 +1,8 @@
 // these are the messages sent around the program. This is divded up here because start.rs imports this
 // as well as the actual music-playing portion of the app
 
+use tokio::sync::mpsc as async_sender;
+
 #[derive(Debug, Clone)]
 pub enum PungeCommand {
     Play,
@@ -16,4 +18,22 @@ pub enum PungeCommand {
     GoToAlbum,  // not implemented yet. will be used as change the surrounding playlist to the album the song is from
     ChangePlaylist(String),  // change the current playlist to the one specified here
     None, // nothing burger
+}
+
+#[derive(Debug, Clone)]
+pub enum ProgramCommands {
+    Test,
+    Send(PungeCommand),
+    UpdateSender(Option<async_sender::UnboundedSender<PungeCommand>>),
+    NewData(String, String, String), // for sending back title, artist and album to GUI
+    VolumeChange(u8),
+    DownloadLink(String),
+    ChangePage(Page)
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Page {
+    Main,
+    Settings,
+    Download
 }
