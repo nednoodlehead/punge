49148f110fdd0f2e0fafe789def314f69fa0b628
NedNoodleHead <spencer.braak@gmail.com>
2023-08-08 20:15:27 -0700

    the download page in question

--- src/gui/download_page.rs
+++ src/gui/download_page.rs
@@ -1,15 +1,36 @@
-use iced::widget::{Container, text, button, column, vertical_space, text_input};
-use iced::{Element};
+use iced::widget::{Container, text, button, column, vertical_space, text_input, row, horizontal_space, scrollable, Scrollable, Column, container, Text};
+use iced::{Element, Length, Alignment};
 use crate::gui::messages::{PungeCommand, ProgramCommands, Page};
 use crate::gui::start::App;
+use crate::gui::scroller;
 
-pub struct DownloadPage;
+pub struct DownloadPage {
+    pub text: String,
+    pub download_feedback: Vec<String>, // feedback to the user to tell them if song was downloaded successfully or not
+}
 
 impl DownloadPage {
     pub fn new() -> Self {
-        DownloadPage
+        DownloadPage {
+            text: "".to_string(),
+            download_feedback: vec!["basic".to_string(), "nut".to_string()],
+        }
     }
     pub fn view(&self) -> Element<'_, ProgramCommands> {
-        Container::new(column![text("Download page"), button(text("Home")).on_press(ProgramCommands::ChangePage(Page::Main))]).into()
+        let input_field = text_input("Paste YouTube links here!", self.text.as_str())
+            .on_input(ProgramCommands::UpdateDownloadEntry).width(Length::Fixed(400.0));
+        let confirm_button = button(text("Download!")).on_press(ProgramCommands::DownloadLink(self.text.clone()));
+        let download_row = row![horizontal_space(Length::Fixed(300.0)),input_field, confirm_button].align_items(Alignment::End);
+        let feedback_scrollable = row![horizontal_space(Length::Fixed(300.0)), container(self.create_scrollable()).style(iced::theme::Container::Custom(Box::from(scroller::ScrollerContainer)))];
+        Container::new(column![text("Download page"), button(text("Home")).on_press(ProgramCommands::ChangePage(Page::Main)), download_row,
+        feedback_scrollable].spacing(10.0)).into()
+    }
+    fn create_scrollable(&self) -> Element<'_, ProgramCommands> { // not the right output type?
+        scrollable(self.download_feedback.iter().fold(
+            Column::new(), |item, string| {
+                item.push(text(string))
+            }
+        )).into()
+
     }
 }
