46677bcfb0abb6e99b978417997c287432540ec1
NedNoodleHead <spencer.braak@gmail.com>
2023-10-07 15:11:04 -0700

    will actually fix this to make it look much nicer and remove the horrors that is start.rs

--- src/gui/subscription.rs
+++ src/gui/subscription.rs
@@ -1,17 +1,41 @@
-// use iced::subscription::{self, Subscription};
-// use iced::futures::channel::mpsc;
-// use iced::futures::sink::SinkExt;
-// use crate::gui::messages::PungeCommand;
-// use crate::player::interface;
-// use crate::playliststructs::PungeMusicObject;
-// use tokio;
+use crate::db::metadata::{on_passive_play, on_seek, skipped_song};
+use crate::gui::messages::{DatabaseMessages, ProgramCommands, PungeCommand};
+use crate::gui::start::App;
+use crate::player::interface;
+use crate::playliststructs::PungeMusicObject;
+use async_std::task::sleep;
+use iced::futures::channel::mpsc;
+use iced::futures::sink::SinkExt;
+use iced::subscription::{self, Subscription};
+use tokio;
 
-
-// pub fn punge_listening_thread() -> Subscription<Self::Message> {
-//         use iced::futures::SinkExt;
-//     iced::subscription::channel(0, 32, |mut sender| async move {
-//         let (gui_send, mut gui_rec) = tokio::sync::mpsc::unbounded_channel();
-//         sender.send(Self::M)
-//     })
-//
-// }
+impl App {
+    // requires a listener. this will be a tokio::UnboundedReceiver<PungeCommand>
+    // does not need 2 way communication, as this subscription just listens and inserts into the database
+    pub fn database_sub(
+        &self,
+        mut db_listener: tokio::sync::mpsc::UnboundedReceiver<DatabaseMessages>,
+    ) -> Subscription<ProgramCommands> {
+        iced::subscription::channel(6, 32, |mut _sender| async move {
+            loop {
+                match db_listener.try_recv() {
+                    Ok(cmd) => match cmd {
+                        DatabaseMessages::Played(uniqueid) => {
+                            on_passive_play(uniqueid).unwrap();
+                        }
+                        DatabaseMessages::Skipped(uniqueid) => {
+                            skipped_song(uniqueid).unwrap();
+                        }
+                        DatabaseMessages::Seeked(uniqueid) => {
+                            on_seek(uniqueid).unwrap();
+                        }
+                    },
+                    Err(e) => {
+                        // ignore!
+                    }
+                }
+                sleep(std::time::Duration::from_millis(50)).await;
+            }
+        })
+    }
+}
