c7621ccb95b2778766e628d04fc29935e9bcc5aa
NedNoodleHead <spencer.braak@gmail.com>
2024-03-04 16:07:08 -0800

    primitive backups

--- src/gui/setting_page.rs
+++ src/gui/setting_page.rs
@@ -1,15 +1,33 @@
-use iced::widget::{Container, text, button, column};
-use iced::{Element};
-use crate::gui::messages::{ProgramCommands, Page};
+use crate::gui::messages::{Page, ProgramCommands};
+use crate::playliststructs::Config;
+use iced::widget::{button, column, row, text, text_input, Container};
+use iced::Element;
 
-
-pub struct SettingPage;
+pub struct SettingPage {
+    pub config: Config,
+    pub backup_text: String,
+}
 
 impl SettingPage {
     pub fn new() -> Self {
-        SettingPage
+        let con = Config {
+            backup_path: String::from(r"%userprofile%/Documents/"),
+        };
+        SettingPage {
+            config: con.clone(),
+            backup_text: con.backup_path.clone(),
+        }
     }
     pub fn view(&self) -> Element<'_, ProgramCommands> {
-        Container::new(column![text("Actual Settings page lol"), button(text("Home")).on_press(ProgramCommands::ChangePage(Page::Main))]).into()
+        Container::new(column![
+            text("Actual Settings page lol"),
+            button(text("Home")).on_press(ProgramCommands::ChangePage(Page::Main)),
+            row![
+                text_input(&self.backup_text, &self.backup_text)
+                    .on_input(ProgramCommands::UpdateBackupText),
+                button(text("Backup!")).on_press(ProgramCommands::CreateBackup)
+            ],
+        ])
+        .into()
     }
 }
---
+++ src/utils/backup.rs
@@ -0,0 +1,20 @@
+use chrono::offset::Local;
+use std::fs::copy;
+pub fn create_backup(mut backup_dir: String) -> Result<(), std::io::Error> {
+    // maybe backup_dir should derive from config? no big deal for now though.
+    backup_dir = if !backup_dir.ends_with("/") | !backup_dir.ends_with("\\") {
+        format!("{}/", backup_dir)
+    } else {
+        backup_dir
+    };
+    println!("{}", &backup_dir);
+    // output should look like: c:/my_backup/punge_backup_2024-01-29-hh-mm-ss.db
+    let output_path = format!(
+        "{}punge_backup{}.db",
+        backup_dir,
+        Local::now().to_string()[..19].replace(":", "-") // get only the important part we want :)
+    );
+    copy(String::from("./main.db"), output_path)?;
+
+    Ok(())
+}
