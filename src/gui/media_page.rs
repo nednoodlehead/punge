8a0db4daac105cb23d3898f04c741f90c9e8936f
NedNoodleHead <spencer.braak@gmail.com>
2024-03-20 08:51:09 -0700

    temp removed the functions

--- src/gui/media_page.rs
+++ src/gui/media_page.rs
@@ -23,31 +23,21 @@ impl MediaPage {
 }
 
 // will need to be a subscription at some point? async too
-fn download_content(link: String, download_path: String) -> String {
-    // string returned is what the user will see, either an error or download success
-    // i dont really see a reason to be able to download playlists like this. who wants to download videos in batch like that? idk idc
-    if link.contains("youtube") {
-        download_youtube(link, download_path);
-    } else if link.contains("instagram") {
-        _download_insta();
-    } else {
-        return "Link does not contain 'instagram' or 'youtube'".to_string();
-    }
-    todo!()
-}
+// fn download_content(link: String, download_path: String) -> String {
+//     // string returned is what the user will see, either an error or download success
+//     // i dont really see a reason to be able to download playlists like this. who wants to download videos in batch like that? idk idc
+//     if link.contains("youtube") {
+//         download_youtube(link, download_path);
+//     } else if link.contains("instagram") {
+//         _download_insta();
+//     } else {
+//         return "Link does not contain 'instagram' or 'youtube'".to_string();
+//     }
+//     todo!()
+// }
 
 // TODO (at some point), make this async and have it send o the subscription that is listening
 // for youtube events. not sure how that will be handled for instagram download...
-fn download_youtube(link: String, _path: String) -> String {
-    let downloaded = rustube::blocking::download_best_quality(&link);
-    match downloaded {
-        Ok(done_path) => {
-            format!("Video has been downloaded to: {:?}", done_path)
-        }
-        Err(e) => {
-            format!("Error downloading: {:?}", e)
-        }
-    }
-}
+// fn download_youtube(link: String, _path: String) -> String {}
 
 fn _download_insta() {}
