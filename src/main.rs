fd217c4588ed1d06e3dac1102d6ffdcbb8554a03
NedNoodleHead <spencer.braak@gmail.com>
2024-04-15 09:21:46 -0700

    db check off of start

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 extern crate core;
-mod types;
-
 mod db;
 mod gui;
 mod player;
+mod types;
 mod utils;
 mod yt;
+use std::path::Path;
 
 fn main() {
+    // if the database is not found
+    if !Path::new("main.db").exists() {
+        db::create_db::create_table_defaults().unwrap();
+    }
     gui::start::begin().unwrap();
 }
